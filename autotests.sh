49a5425cf1503931b5aac33449cd20df199ea3c7
Igor Shulga Ubuntu <shulganew@gmail.com>
2023-11-25 17:48:48 +0300

    Modify gitignore

--- autotests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-go build -o ./cmd/shortener/shortener ./cmd/shortener/main.go
-#go vet -vettool=$(which statictest) ./...
-shortenertestbeta -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
-#shortenertestbeta -test.v -test.run=^TestIteration7$ -binary-path=cmd/shortener/shortener -source-path=.
-#TEMP_FILE=$(random tempfile)
-#shortenertestbeta -test.v -test.run=^TestIteration9$  -binary-path=cmd/shortener/shortener -source-path=. -file-storage-path=$TEMP_FILE
